013b88292c5f700667280a3e37163c8ad13c736c
stas <uni@staslange.de>
2019-09-26 17:18:04 +0900

    minor changes for demo

--- python-flask-server/swagger_server/controllers/actions_controller.py
+++ python-flask-server/swagger_server/controllers/actions_controller.py
@@ -1,9 +1,19 @@
 import connexion
 import six
+from threading import Timer
 
 from swagger_server.models.body import Body  # noqa: E501
 from swagger_server.models.route import Route  # noqa: E501
 from swagger_server import util
+from swagger_server.controllers.sfcr_controller import get_active_requests
+import module_client_trafgen.swagger_client_trafgen as swagc_trafgen
+
+
+def notify_trafgen(sfcr):
+    print("[ actions_controller ] Finished infrastructure setup, notifying trafgen.\n")
+    cfg = swagc_trafgen.Configuration()
+    trafgen_api_instance = swagc_trafgen.TrafgenApi(swagc_trafgen.ApiClient(cfg))
+    trafgen_api_instance.add_sfcr(sfcr)
 
 
 def deploy_vnf(body):  # noqa: E501
@@ -20,6 +30,9 @@ def deploy_vnf(body):  # noqa: E501
         body = Body.from_dict(connexion.request.get_json())  # noqa: E501
         print("[ actions_controller ] Received deployment request: %s.\n" % str(body))
         print("[ actions_controller ] Deploying VNF %s on node id %d.\n" % (body.flavor.name, body.node))
+        current_sfcr = get_active_requests()[-1]
+        t = Timer(3, notify_trafgen, [current_sfcr])
+        t.start()
     return 'do some magic!'
 
 
