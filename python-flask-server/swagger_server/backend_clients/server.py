a43d815cb1c37e4590e76d39f4560e7824751ba7
tu-nv <vantu.bkhn@gmail.com>
2019-10-02 12:46:05 +0900

    refactor openstack client

--- python-flask-server/swagger_server/backend_clients/server.py
+++ python-flask-server/swagger_server/backend_clients/server.py
@@ -3,29 +3,17 @@ import requests
 import uuid
 
 from swagger_server.config import cfg
-from swagger_server.backend_clients.utils import create_openstack_client, get_base_urls
+from swagger_server.backend_clients.utils import openstack_client as client
 
-auth_cfg = cfg["openstack_client"]["auth"]
 vnf_cfg = cfg["openstack_client"]["vnf"]
 
-_client = create_openstack_client(**auth_cfg)
-_base_urls = get_base_urls(_client)
-
-def rset_auth_info():
-    # FIXME: after the token is expired(?), we should re-authenticate.
-    # however, the auth_url, username and password is also lost,
-    # causing error on  re-authenticate call. A dirty fix
-    # is to set them every call.
-    _client.auth_url = auth_cfg["auth_url"]
-    _client.username = auth_cfg["username"]
-    _client.password = auth_cfg["password"]
 
 def create_server(flavor_id, host_name):
-    rset_auth_info()
+    client.rset_auth_info()
 
-    base_url = _base_urls["compute"]
+    base_url = client.base_urls["compute"]
     url = "/servers"
-    headers = {'X-Auth-Token': _client.auth_token}
+    headers = {'X-Auth-Token': client.client.auth_token}
 
     data = {
                 "server": {
@@ -47,11 +35,11 @@ def create_server(flavor_id, host_name):
         return req.json(), req.status_code
 
 def stop_server(server_id):
-    rset_auth_info()
+    client.rset_auth_info()
 
-    base_url = _base_urls["compute"]
+    base_url = client.base_urls["compute"]
     url = "/servers/{}/action".format(server_id)
-    headers = {'X-Auth-Token': _client.auth_token}
+    headers = {'X-Auth-Token': client.client.auth_token}
 
     data = {
                 "os-stop" : "dummy"
--- python-flask-server/swagger_server/backend_clients/utils.py
+++ python-flask-server/swagger_server/backend_clients/utils.py
@@ -3,17 +3,36 @@ from keystoneauth1.identity import v3
 from keystoneauth1 import session
 from keystoneclient.v3 import client as keystone_client
 
-def create_openstack_client(*args, **kwargs):
-    auth = v3.Password(*args, **kwargs)
-    sess = session.Session(auth=auth)
-    client = keystone_client.Client(session=sess)
-    client.authenticate(*args, **kwargs)
-    return client
+from swagger_server.config import cfg
 
-def get_base_urls(client):
-    base_urls = dict()
-    endpoints = client.endpoints.list()
-    for endpoint in endpoints:
-        service = client.services.get(endpoint.service_id)
-        base_urls[service.type] = endpoint.url
-    return base_urls
+class OpenstackClient():
+    def __init__(self, auth_cfg):
+        self.auth_cfg = auth_cfg
+        self.client = self._create_openstack_client()
+        self.base_urls = self._get_base_urls()
+
+    def _create_openstack_client(self):
+        auth = v3.Password(**self.auth_cfg)
+        sess = session.Session(auth=auth)
+        client = keystone_client.Client(session=sess)
+        client.authenticate(**self.auth_cfg)
+        return client
+
+    def _get_base_urls(self):
+        base_urls = dict()
+        endpoints = self.client.endpoints.list()
+        for endpoint in endpoints:
+            service = self.client.services.get(endpoint.service_id)
+            base_urls[service.type] = endpoint.url
+        return base_urls
+
+    def rset_auth_info(self):
+        # FIXME: after the token is expired(?), we should re-authenticate.
+        # however, the auth_url, username and password is also lost,
+        # causing error on  re-authenticate call. A dirty fix
+        # is to set them every call.
+        self.client.auth_url = self.auth_cfg["auth_url"]
+        self.client.username = self.auth_cfg["username"]
+        self.client.password = self.auth_cfg["password"]
+
+openstack_client = OpenstackClient(cfg["openstack_client"]["auth"])
