c7b9593573e1c0102b1c5a3f40832872e00e5932
tu-nv <vantu.bkhn@gmail.com>
2019-10-07 18:20:20 +0900

    remove get topology, placement, and vnfflavor get from ni_mon instead so we dont need to maintain duplicated code

--- nfvo_server/nfvo_server/models/__init__.py
+++ nfvo_server/nfvo_server/models/__init__.py
@@ -3,15 +3,8 @@
 # flake8: noqa
 from __future__ import absolute_import
 # import models into model package
-from nfvo_server.models.assignment import Assignment
 from nfvo_server.models.body import Body
-from nfvo_server.models.link import Link
-from nfvo_server.models.monitoring_entry import MonitoringEntry
-from nfvo_server.models.node import Node
 from nfvo_server.models.route import Route
 from nfvo_server.models.sfcr import SFCR
 from nfvo_server.models.shutdown import Shutdown
-from nfvo_server.models.topology import Topology
 from nfvo_server.models.vnf_flavor import VNFFlavor
-from nfvo_server.models.vnf_instance import VNFInstance
-from nfvo_server.models.vnf_type import VNFType
--- nfvo_server/nfvo_server/test/test_info_controller.py
+++ nfvo_server/nfvo_server/test/test_info_controller.py
@@ -7,26 +7,11 @@ from six import BytesIO
 
 from nfvo_server.models.route import Route  # noqa: E501
 from nfvo_server.models.sfcr import SFCR  # noqa: E501
-from nfvo_server.models.topology import Topology  # noqa: E501
-from nfvo_server.models.vnf_flavor import VNFFlavor  # noqa: E501
-from nfvo_server.models.vnf_instance import VNFInstance  # noqa: E501
 from nfvo_server.test import BaseTestCase
 
 
 class TestInfoController(BaseTestCase):
     """InfoController integration test stubs"""
-
-    def test_get_placement(self):
-        """Test case for get_placement
-
-        Get current placement information, i.e., list of all active VNF instances including their location.
-        """
-        response = self.client.open(
-            '/v2/placement',
-            method='GET',
-            content_type='application/json')
-        self.assert200(response,
-                       'Response body is : ' + response.data.decode('utf-8'))
 
     def test_get_requests(self):
         """Test case for get_requests
@@ -47,30 +32,6 @@ class TestInfoController(BaseTestCase):
         """
         response = self.client.open(
             '/v2/routes',
-            method='GET',
-            content_type='application/json')
-        self.assert200(response,
-                       'Response body is : ' + response.data.decode('utf-8'))
-
-    def test_get_topology(self):
-        """Test case for get_topology
-
-        Get current topology information.
-        """
-        response = self.client.open(
-            '/v2/topology',
-            method='GET',
-            content_type='application/json')
-        self.assert200(response,
-                       'Response body is : ' + response.data.decode('utf-8'))
-
-    def test_get_vnf_flavors(self):
-        """Test case for get_vnf_flavors
-
-        Get available VNF flavors.
-        """
-        response = self.client.open(
-            '/v2/vnfflavors',
             method='GET',
             content_type='application/json')
         self.assert200(response,
