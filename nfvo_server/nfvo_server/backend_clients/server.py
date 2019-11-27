215aeab74653c5d9a2d561339bb443ca5f3e5b4b
tu-nv <vantu.bkhn@gmail.com>
2019-11-27 01:44:07 +0000

    each openstack vnf flavor has it own base_image_id in extra specs

--- nfvo_server/nfvo_server/backend_clients/server.py
+++ nfvo_server/nfvo_server/backend_clients/server.py
@@ -12,9 +12,21 @@ vnf_cfg = cfg["openstack_client"]["vnf"]
 def create_server(server_prefix, flavor_id, host_name, user_data):
     client.rset_auth_info()
 
+    # extra specs API link: https://github.com/openstack/nova/blob/master/api-ref/source/os-flavor-extra-specs.inc
     base_url = client.base_urls["compute"]
-    url = "/servers"
     headers = {'X-Auth-Token': client.client.auth_token}
+
+    url = "/flavors/{flavors_id}/os-extra_specs".format(flavors_id=flavor_id)
+    req = requests.get("{}{}".format(base_url, url),
+        headers=headers)
+    if not req.status_code == 200:
+        return req.json(), req.status_code
+
+    extra_specs = req.json()
+    try:
+        os_image_id = extra_specs["extra_specs"]["os_image_id"]
+    except Exception as e:
+        return "Cannot find os_image_id in flavor extra specs", 404
 
     if server_prefix:
         server_name = "{}_{}".format(server_prefix, str(uuid.uuid4()))
@@ -24,7 +36,7 @@ def create_server(server_prefix, flavor_id, host_name, user_data):
     data = {
                 "server": {
                     "name" : server_name,
-                    "imageRef" : vnf_cfg["base_image_id"],
+                    "imageRef" : os_image_id,
                     "flavorRef" : flavor_id,
                     "availability_zone": "nova:{}".format(host_name),
                     "user_data" : base64.b64encode(user_data.encode('ascii')),
@@ -35,6 +47,7 @@ def create_server(server_prefix, flavor_id, host_name, user_data):
                 }
             }
 
+    url = "/servers"
     req = requests.post("{}{}".format(base_url, url),
         json=data,
         headers=headers)
