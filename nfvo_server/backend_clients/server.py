4d3b1dc1288b9d91116f2fd36d635abd8dbb5c15
tu-nv <vantu.bkhn@gmail.com>
2019-12-09 12:19:19 +0900

    put default user data per vnf flavor

--- nfvo_server/backend_clients/server.py
+++ nfvo_server/backend_clients/server.py
@@ -9,7 +9,7 @@ from nfvo_server.backend_clients.utils import openstack_client as client
 vnf_cfg = cfg["openstack_client"]["vnf"]
 
 
-def create_server(server_prefix, flavor_id, host_name, user_data):
+def create_server(server_prefix, flavor_id, host_name, custom_user_data):
     client.rset_auth_info()
 
     # extra specs API link: https://github.com/openstack/nova/blob/master/api-ref/source/os-flavor-extra-specs.inc
@@ -25,8 +25,16 @@ def create_server(server_prefix, flavor_id, host_name, user_data):
     extra_specs = req.json()
     try:
         os_image_id = extra_specs["extra_specs"]["os_image_id"]
+        default_user_data = extra_specs["extra_specs"].get("default_user_data")
     except Exception as e:
         return "Cannot find os_image_id in flavor extra specs", 404
+
+    if custom_user_data is not None:
+        user_data = base64.b64encode(custom_user_data.encode('ascii'))
+    else:
+        # store user_data in extra_specs convert \n to \\n. need to restore
+        default_user_data = default_user_data.replace('\\n', '\n')
+        user_data = base64.b64encode(default_user_data.encode('ascii'))
 
     if server_prefix:
         server_name = "{}_{}".format(server_prefix, str(uuid.uuid4()))
@@ -39,7 +47,7 @@ def create_server(server_prefix, flavor_id, host_name, user_data):
                     "imageRef" : os_image_id,
                     "flavorRef" : flavor_id,
                     "availability_zone": "nova:{}".format(host_name),
-                    "user_data" : base64.b64encode(user_data.encode('ascii')),
+                    "user_data" : user_data,
                     "networks": [
                         {"uuid": vnf_cfg["mgmt_net_id"]},
                         {"uuid": vnf_cfg["data_net_id"]},
